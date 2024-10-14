de7eb3b5d43319bca1c58f73fa1ce40f7b676db7
Imani Walker <imaniwalker0305@gmail.com>
2024-10-13 23:11:28 -0400

    Connecting News api

--- Digital_Fusion/src/components/ArticalCards/ArticalCard.jsx
+++ Digital_Fusion/src/components/ArticalCards/ArticalCard.jsx
@@ -1,32 +1,95 @@
-// import React, { useState } from 'react';
-// const [articles, setArticles] = useState([]);
-// const [isLoading, setIsLoading] = useState(false);
-// const [error, setError] = useState(null);
-function ArticalCards() {
+import React, { useEffect, useState } from "react";
+import { API_KEY, BASE_URL } from "../../utils/NewsApi";
+
+const ArticalCards = () => {
+  const [articles, setArticles] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [query, setQuery] = useState("");
+
+  const getDates = () => {
+    const today = new Date();
+    const fromDate = new Date();
+    fromDate.setDate(today.getDate() - 7);
+
+    return {
+      from: fromDate.toISOString().split("T")[0],
+      to: today.toISOString().split("T")[0],
+    };
+  };
+
+  useEffect(() => {
+    const fetchNews = () => {
+      const { from, to } = getDates();
+      const url = `${BASE_URL}?q=${query}&from=${from}&to=${to}&pageSize=100&apiKey=${API_KEY}`;
+
+      fetch(url)
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error(`Cannot fetch data: ${response.status}`);
+          }
+          return response.json();
+        })
+        .then((data) => {
+          setArticles(data.articles);
+          setLoading(false);
+        })
+        .catch((err) => {
+          setError(err);
+          setLoading(false);
+        });
+    };
+
+    if (query) {
+      setLoading(true);
+      fetchNews();
+    }
+  }, [query]);
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    setLoading(true);
+  };
+
   return (
-    <>
-      <div className="artical__card_section">
-        <div className="artical__card">
-          <img src="" className="artical__card_image" />
-          <div className="artical__card_content">
-            <h1 className="artical__card_date"></h1>
-            <h2 className="arrtical__card_title"></h2>
-            <p className="artical__card_description"></p>
-            <p className="artical__card_source"></p>
-          </div>
-        </div>
+    <div>
+      <h1>Tech News</h1>
+      <form onSubmit={handleSearch}>
+        <input
+          type="text"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+          placeholder="Search for news"
+          minLength={2}
+          required
+        />
+
+        <button type="submit">Search</button>
+      </form>
+
+      {loading && <div>Loading...</div>}
+      {error && <div>Error fetching news: {error.message}</div>}
+
+      <div className="artical__card">
+        {articles.map((article, articleId) => (
+          <li key={articleId}>
+            <h2>{article.title}</h2>
+            {article.urlToImage && (
+              <img
+                src={article.urlToImage}
+                alt={article.title}
+                className="artical__image"
+              />
+            )}
+            <p className="artical__paragraph">{article.description}</p>
+            <a href={article.url} target="_blank" className="artical__link">
+              Read more
+            </a>
+          </li>
+        ))}
       </div>
-    </>
+    </div>
   );
-}
-// export default ArticalCards;
-// const ArticleCard = ({ article }) => (
-//     <div className="article-card">
-//         <img src={article.urlToImage} alt={article.title} />
-//         <h3>{article.title}</h3>
-//         <p>{article.description}</p>
-//         <div>Source: {article.source.name}</div>
-//         <div>Date: {new Date(article.publishedAt).toLocaleDateString()}</div>
-//     </div>
-// );
+};
+
 export default ArticalCards;
--- Digital_Fusion/src/components/Main/Main.jsx
+++ Digital_Fusion/src/components/Main/Main.jsx
@@ -1,6 +1,5 @@
 import "../Main/Main.css";
 import React from "react";
-
 import { Card } from "../Card/Card";
 import { useEffect, useState } from "react";
 export function Main() {
@@ -25,7 +24,6 @@ export function Main() {
         <div className="main__container">
           <h1 className="main__text">Bring your Dreams to life</h1>
         </div>
-        {/* {isScrolled && <h1 className="main__header">How it works</h1>} */}
         <h1 className={`main__header ${isScrolled ? "visible" : "hidden"}`}>
           How it works
         </h1>
