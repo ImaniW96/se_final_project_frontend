de98f37cf558bc25033b99668e15851e9d8d219d
Imani Walker <imaniwalker0305@gmail.com>
2024-10-16 21:45:04 -0400

    Style and font update

--- Digital_Fusion/src/components/App/App.jsx
+++ Digital_Fusion/src/components/App/App.jsx
@@ -1,21 +1,25 @@
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 // import { Routes, Route, Navigate, useNavigate } from "react-router-dom";
 import Community from "../Community/Community";
 import Technews from "../Technews/Technews";
 import Mentor from "../Mentor/Mentor";
 import { Main } from "../Main/Main";
+import { Header } from "../Header/Header";
+import { Footer } from "../Footer/Footer";
 
 function App() {
   return (
-    <Router>
+    <>
+      <Header />
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/community" element={<Community />} />
         <Route path="/mentor" element={<Mentor />} />
         <Route path="/technews" element={<Technews />} />
       </Routes>
-    </Router>
+      <Footer />
+    </>
   );
 }
 
--- Digital_Fusion/src/components/Header/Header.jsx
+++ Digital_Fusion/src/components/Header/Header.jsx
@@ -1,39 +1,129 @@
+// import "../Header/Header.css";
+// import { useNavigate } from "react-router-dom";
+// export function Header() {
+//   const navigate = useNavigate();
+
+//   return (
+//     <>
+//       <header className="header">
+//         <ul className="header__item_container">
+//           <div>
+//             {" "}
+//             <button className="header__logo" onClick={() => navigate("/")}>
+//               {" "}
+//               Digital Fusion
+//             </button>
+//           </div>
+//           <button className="header__item" onClick={() => navigate("/Mentor")}>
+//             Become a Mentor
+//           </button>
+
+//           <button
+//             className="header__item"
+//             onClick={() => navigate("/Community")}
+//           >
+//             Community
+//           </button>
+//           <button
+//             className="header__item"
+//             onClick={() => navigate("/UxDesign")}
+//           >
+//             Ux Design
+//           </button>
+//           <button
+//             className="header__item"
+//             onClick={() => navigate("/SoftwareEngineer")}
+//           >
+//             Software Engineering
+//           </button>
+//           <button
+//             className="header__item"
+//             onClick={() => navigate("/Marketing")}
+//           >
+//             Marketing
+//           </button>
+//           <button className="header__item" onClick={() => navigate("/Aboutus")}>
+//             About Us
+//           </button>
+
+//           <button
+//             className="header__item"
+//             onClick={() => navigate("/TechNews")}
+//           >
+//             Tech News
+//           </button>
+
+//           <button className="header__item" onClick={() => navigate("/Donate")}>
+//             Donate
+//           </button>
+//         </ul>
+//       </header>
+//     </>
+//   );
+// }
 import "../Header/Header.css";
-// import { Community } from "../Community/Community";
-// import React, { useState, useEffect, useRef } from "react";
+import { useNavigate } from "react-router-dom";
+
 export function Header() {
-  //   const [isOpen, setIsOpen] = useState(false);
-  //   const toggleDropDown = () => {
-  //     setIsOpen((prev) => !prev);
-  //   };
-  //   const handleClickOutside = (event) => {
-  //     if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
-  //       setIsOpen(false);
-  //     }
-  //   };
-  //   useEffect(() => {
-  //     document.addEventListener("mousedown", handleClickOutside);
-  //     return () => {
-  //       document.removeEventListener("mousedown", handleClickOutside);
-  //     };
-  //   }, []);
+  const navigate = useNavigate();
 
   return (
     <>
       <header className="header">
-        {/* <div className="header__dropdown">
-          <button onClick={toggleDropDown}>=</button>
-        </div> */}
-
         <ul className="header__item_container">
-          <button className="header__item">Become a Mentor</button>
-          <button className="header__item">Community</button>
-          <button className="header__item">Ux Design</button>
-          <button className="header__item">Software Engineering</button>
-          <button className="header__item">Marketing</button>
-          <button className="header__item">About Us</button>
-          <button className="header__item">Tech News</button>
-          <button className="header__item">Donate</button>
+          <button className="header__logo" onClick={() => navigate("/")}>
+            Digital Fusion
+          </button>
+          <div className="header__menu">
+            <button
+              className="header__item"
+              onClick={() => navigate("/Mentor")}
+            >
+              Become a Mentor
+            </button>
+            <button
+              className="header__item"
+              onClick={() => navigate("/Community")}
+            >
+              Community
+            </button>
+            <button
+              className="header__item"
+              onClick={() => navigate("/UxDesign")}
+            >
+              Ux Design
+            </button>
+            <button
+              className="header__item"
+              onClick={() => navigate("/SoftwareEngineer")}
+            >
+              Software Engineering
+            </button>
+            <button
+              className="header__item"
+              onClick={() => navigate("/Marketing")}
+            >
+              Marketing
+            </button>
+            <button
+              className="header__item"
+              onClick={() => navigate("/Aboutus")}
+            >
+              About Us
+            </button>
+            <button
+              className="header__item"
+              onClick={() => navigate("/TechNews")}
+            >
+              News
+            </button>
+            <button
+              className="header__item"
+              onClick={() => navigate("/Donate")}
+            >
+              Donate
+            </button>
+          </div>
         </ul>
       </header>
     </>
