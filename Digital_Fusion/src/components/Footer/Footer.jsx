1b7ce481bdf0b964a694f9570f5b1ce9105a7907
Imani Walker <imaniwalker0305@gmail.com>
2024-10-19 18:21:56 -0400

    Copyright

--- Digital_Fusion/src/components/Footer/Footer.jsx
+++ Digital_Fusion/src/components/Footer/Footer.jsx
@@ -9,6 +9,7 @@ export function Footer() {
           <button className="footer__item">Tik Tok</button>
           <button className="footer__item">Terms of Service</button>
           <button className="footer__item">Contact Us</button>
+          <p>&copy; 2024 Digital Fusion All rights reserved.</p>
         </ul>
       </footer>
     </>
