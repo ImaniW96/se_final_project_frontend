6909b1a896042389cf03414d828eff8263cca7bd
Imani Walker <imaniwalker0305@gmail.com>
2024-10-19 22:14:04 -0400

    header update

--- Digital_Fusion/vite.config.js
+++ Digital_Fusion/vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/se_final_project_frontend",
+  base: "/se_final_project_frontend/",
   plugins: [react()],
 });
