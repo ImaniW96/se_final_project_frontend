e24aff3cd9f5a59e90f257d459c5a75cbe032639
Imani Walker <imaniwalker0305@gmail.com>
2024-11-12 12:10:38 -0500

    About update

--- Digital_Fusion/src/components/App/App.jsx
+++ Digital_Fusion/src/components/App/App.jsx
@@ -6,18 +6,21 @@ import Mentor from "../Mentor/Mentor";
 import { Main } from "../Main/Main";
 import { Header } from "../Header/Header";
 import { Footer } from "../Footer/Footer";
+import AboutUs from "../AboutUs/AboutUs";
 
 function App() {
   return (
     <>
       <Header />
+
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/community" element={<Community />} />
         <Route path="/mentor" element={<Mentor />} />
         <Route path="/technews" element={<Technews />} />
+        <Route path="/aboutus" element={<AboutUs />} />
       </Routes>
-      <Footer />
+        <Footer />
     </>
   );
 }
--- Digital_Fusion/src/components/Header/Header.jsx
+++ Digital_Fusion/src/components/Header/Header.jsx
@@ -64,7 +64,7 @@ export function Header() {
               </button>
               <button
                 className="header__item"
-                onClick={() => navigate("/Aboutus")}
+                onClick={() => navigate("/AboutUs")}
               >
                 About Us
               </button>
